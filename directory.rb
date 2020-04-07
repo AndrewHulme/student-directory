1d2eb1aaa5f0d0bb248301e48993e2b2dd4cab6a
Andrew Hulme <andrewhulme1993@gmail.com>
2020-04-07 10:44:50 +0000

    Added more info to hash and centered output

--- directory.rb
+++ directory.rb
@@ -1,20 +1,22 @@
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the names of the students and their hobby"
   puts "To finish, just hit return twice"
   students = []
   name = gets.chomp
+  hobby = gets.chomp
 
   while !name.empty? do
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: :november, hobby: hobby, country: :England}
     puts "Now we have #{students.count} students"
     name = gets.chomp
+    hobby = gets.chomp
   end
   students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------" 
+  puts ("The students of Villains Academy").center(180)
+  puts ("-------------").center(180) 
 end
 
 def print(students)
@@ -22,14 +24,14 @@ def print(students)
   
   while counter < students.count
     if students[counter][:name][0] == "A" && students[counter][:name].length < 12
-      puts "#{counter + 1}: #{students[counter][:name]} (#{students[counter][:cohort]} cohort)"
+      puts ("#{counter + 1}: #{students[counter][:name]} - Hobby: #{students[counter][:hobby]}, Country of Origin: #{students[counter][:country]} (#{students[counter][:cohort]} cohort)").center(180)
     end
       counter += 1
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts ("Overall, we have #{students.count} great students").center(180)
 end
 
 students = input_students

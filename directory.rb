5818a3bc5dc97774629c03072f71ef64999d04e2
Andrew Hulme <andrewhulme1993@gmail.com>
2020-04-07 09:24:15 +0000

    Only print names under certain circumstances

--- directory.rb
+++ directory.rb
@@ -1,20 +1,6 @@
-def print_header
-  puts "The students of Villains Academy"
-  puts "-------------" 
-end
-def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end
-end
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
-end
-
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-
   students = []
   name = gets.chomp
 
@@ -23,9 +9,28 @@ def input_students
     puts "Now we have #{students.count} students"
     name = gets.chomp
   end
-  
   students
 end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------" 
+end
+
+def print(students)
+  students.each_with_index do |student, index|
+    
+    if student[:name][0] == "A" && student[:name].length < 12
+      puts "#{index + 1}: #{student[:name]} (#{student[:cohort]} cohort)"
+    end
+    
+  end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+
 
 students = input_students
 print_header
